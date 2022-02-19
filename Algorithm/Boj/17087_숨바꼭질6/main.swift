51de4f80e1aae68c7dd12fd8b4ff5bc465d63935
Jisoo <dlwltn815@gmail.com>
2022-02-19 15:39:33 +0900

    17087 숨바꼭질6 문제 풀이 완료

--- Algorithm/Boj/17087_숨바꼭질6/main.swift
+++ Algorithm/Boj/17087_숨바꼭질6/main.swift
@@ -7,6 +7,7 @@
 
 let input = readLine()!.split(separator: " ").map { Int(String($0))! }
 var bro = readLine()!.split(separator: " ").map { Int(String($0))! }
+var distance: [Int] = []
 
 func gcd(_ m: Int, _ n: Int) -> Int {
     if n == 0 {
@@ -18,7 +19,17 @@ func gcd(_ m: Int, _ n: Int) -> Int {
     }
 }
 
-
 for b in bro {
-    
+    distance.append(abs(input[1]-b))
 }
+
+var ans = distance[0]
+for i in 1..<distance.count {
+    if ans < distance[i] {
+        ans = gcd(distance[i], ans)
+    } else {
+        ans = gcd(ans, distance[i])
+    }
+}
+
+print(ans)
