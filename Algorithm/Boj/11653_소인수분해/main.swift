1594483b85064f35ae8515e62d4525e7fb902659
Jisoo <dlwltn815@gmail.com>
2022-02-28 23:27:04 +0900

    11653 소인수분해 문제 풀이 완료

--- Algorithm/Boj/11653_소인수분해/main.swift
+++ Algorithm/Boj/11653_소인수분해/main.swift
@@ -10,12 +10,13 @@ import Foundation
 var num = Int(readLine()!)!
 let n = Int(sqrt(Double(num)))
 
-for i in 2..<n {
+for i in stride(from: 2, to: num, by: 1) {
     while num % i == 0 {
         num /= i
         print(i)
     }
 }
+
 
 if num != 1 {
     print(num)
