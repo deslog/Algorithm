fc5fc329482bd75153d359e62aea6859ff2efa2f
Jisoo <dlwltn815@gmail.com>
2022-03-05 15:39:25 +0900

    11727 2xn타일링2 런타임에러 해결

--- Algorithm/Boj/11727_2xn타일링2/main.swift
+++ Algorithm/Boj/11727_2xn타일링2/main.swift
@@ -9,15 +9,15 @@
 let n = Int(readLine()!)!
 var dp = [Int](repeating: 0, count: n+1)
 
-if n == 2 {
+if n == 1 {
+    print(1)
+} else if n == 2 {
     print(3)
-} else if n == 3 {
-    print(5)
 } else {
+    dp[1] = 1
     dp[2] = 3
-    dp[3] = 5
     
-    for i in 4..<n+1 {
+    for i in 3..<n+1 {
         dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007
     }
     print(dp[n])
