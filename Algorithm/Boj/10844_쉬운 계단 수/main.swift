730a87efc31027a142dfc23fddec2ad9b557a95f
Jisoo <dlwltn815@gmail.com>
2022-03-12 14:35:55 +0900

    10844 쉬운 계단 수 문제 풀이

---
+++ Algorithm/Boj/10844_쉬운 계단 수/main.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 이지수 on 2022/03/12.
+//
+
+let n = Int(readLine()!)!
+var dp = [[Int]](repeating: Array(repeating: 0, count: 10), count: n+1)
+var sum = 0
+
+for i in 0...9 {
+    dp[1][i] = 1
+}
+
+if n > 1 {
+    for i in 2...n {
+        for j in 0...9 {
+            if j == 0 {
+                dp[i][j] = dp[i-1][j+1] % 1000000000
+            } else if j == 9 {
+                dp[i][j] = dp[i-1][j-1] % 1000000000
+            } else {
+                dp[i][j] = (dp[i-1][j-1] + dp [i-1][j+1]) % 1000000000
+            }
+        }
+    }
+}
+
+for i in 1...9 {
+    sum = (sum + dp[n][i]) % 1000000000
+}
+
+
+print(sum%1000000000)
+
