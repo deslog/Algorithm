a80e8a1282406619a4b867ca097c46ad4f76e52e
Jisoo <dlwltn815@gmail.com>
2022-03-19 00:15:21 +0900

    15988 코드 정리, 조건 추가  --> 런타임 에러

--- Algorithm/Boj/15988_1,2,3더하기 3/main.swift
+++ Algorithm/Boj/15988_1,2,3더하기 3/main.swift
@@ -10,17 +10,14 @@ var input: [Int] = []
 for _ in 0..<n {
     input.append(Int(readLine()!)!)
 }
-var dp = Array(repeating: 0, count: input.max()!+1)
 
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
-
-for i in 4..<input.max()!+1 {
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+for i in 0..<n {
+    var dp = Array(repeating: 0, count: input[i]+1)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    for j in 4..<input[i]+1 {
+        dp[j] = (dp[j-1] + dp[j-2] + dp[j-3]) % 1000000009
+    }
+    print(dp[input[i]])
 }
-
-for i in input {
-    print(dp[i])
-}
-
