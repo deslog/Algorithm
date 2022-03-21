651748fdb463f3f30558e0ecb8ec970ba74696b3
Jisoo <dlwltn815@gmail.com>
2022-03-21 23:50:02 +0900

    11057 틀렸습니다. (1처리 안해줘서 틀린듰)

--- Algorithm/Boj/11057_오르막 수/main.swift
+++ Algorithm/Boj/11057_오르막 수/main.swift
@@ -5,4 +5,25 @@
 //  Created by 이지수 on 2022/03/21.
 //
 
-import Foundation
+let n = Int(readLine()!)!
+var dp = Array(repeating: Array(repeating: 0, count: 10), count: n+1)
+
+// 한자리수는 무조건 1개씩
+for i in 0...9 {
+    dp[1][i] = 1
+}
+
+for i in 2..<n+1 {
+    for j in 0...9 {
+        for k in j...9 {
+            dp[i][k] += dp[i-1][j]
+            dp[i][k] %= 10007
+        }
+    }
+}
+
+func sum(_ numbers: [Int]) -> Int {
+    return numbers.reduce(0, +)
+}
+
+print(sum(dp[n]))
