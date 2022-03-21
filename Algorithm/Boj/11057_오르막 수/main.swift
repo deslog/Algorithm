72703dc7c9c1ff70201acd4bac462f09781a69aa
Jisoo <dlwltn815@gmail.com>
2022-03-21 23:57:19 +0900

    11057 문제 해결 (마지막 나누기 조건을 안씀)

--- Algorithm/Boj/11057_오르막 수/main.swift
+++ Algorithm/Boj/11057_오르막 수/main.swift
@@ -8,22 +8,25 @@
 let n = Int(readLine()!)!
 var dp = Array(repeating: Array(repeating: 0, count: 10), count: n+1)
 
+//sum 함수 구현
+func sum(_ numbers: [Int]) -> Int {
+    return numbers.reduce(0, +)
+}
+
 // 한자리수는 무조건 1개씩
 for i in 0...9 {
     dp[1][i] = 1
 }
 
-for i in 2..<n+1 {
-    for j in 0...9 {
-        for k in j...9 {
-            dp[i][k] += dp[i-1][j]
-            dp[i][k] %= 10007
+if n ==  1 {
+    print(sum(dp[1]))
+} else {
+    for i in 2..<n+1 {
+        for j in 0...9 {
+            for k in j...9 {
+                dp[i][j] = (dp[i][j] + dp[i-1][k]) % 10007
+            }
         }
     }
+    print(sum(dp[n])%10007)
 }
-
-func sum(_ numbers: [Int]) -> Int {
-    return numbers.reduce(0, +)
-}
-
-print(sum(dp[n]))
