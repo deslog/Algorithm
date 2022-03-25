5507b1eba159cd5ddcfe37841dd16ef68d1a4759
Jisoo <dlwltn815@gmail.com>
2022-03-26 03:40:36 +0900

    2156 포도주 시식 런타임에러 해결 (조건추가)

--- Algorithm/Boj/2156_포도주시식/main.swift
+++ Algorithm/Boj/2156_포도주시식/main.swift
@@ -8,15 +8,23 @@
 let n = Int(readLine()!)!
 var podoju: [Int] = []
 var dp = Array(repeating: 0, count: n+1)
+podoju.append(0)
+
 for _ in 1...n {
     podoju.append(Int(readLine()!)!)
 }
 
 dp[1] = podoju[1]
-dp[2] = max(dp[1], dp[1] + podoju[1])
 
-for i in 3..<n+1 {
-    dp[i] = max(dp[i-1], dp[i-3] + podoju[i-1] + podoju[i], dp[i-2] + podoju[i])
+if n > 1 {
+    dp[2] = podoju[2]+podoju[1]
+}
+
+if n > 2{
+    for i in 3..<n+1 {
+        dp[i] = max(dp[i-1], dp[i-3] + podoju[i-1] + podoju[i], dp[i-2] + podoju[i])
+    }
 }
 
 print(dp[n])
+
