d9838e62490fb5f28f05e4af391d5edf3130aa1b
JI SOO <dlwltn815@gmail.com>
2022-04-17 16:10:07 +0900

    1107 리모컨 문제풀이 완료 (브루트포스, 다시 풀어보기, 예외 생각하기)

--- Algorithm/Boj/1107_리모컨/main.swift
+++ Algorithm/Boj/1107_리모컨/main.swift
@@ -7,7 +7,10 @@
 
 var n = Int(readLine()!)!
 var num = Int(readLine()!)!
-var breakBtn = readLine()!.split(separator: " ").map { Int(String(&0)) }
+var breakBtn: [Int] = []
+if num != 0 {
+    breakBtn = readLine()!.split(separator: " ").map{Int(String($0))!}
+}
 
 // 채널로 도착할 수 있는 방법은 세가지
 // 1. +-로만 이동
@@ -56,35 +59,34 @@ func check(_ i: Int) -> Int{
 
 
 
-/* python 참고 코드
-min_cnt = 0x3f3f3f3f
-num_int = 0
-btn_set = {i for i in range(0, 10)}
-
-
-def find(num):
-    global min_cnt, num_int, btn_set
-
-    for btn in btn_set:
-        tmp_num = num + str(btn)
-        min_cnt = min(min_cnt, abs(num_int - int(tmp_num)) + len(tmp_num))
-
-        if len(tmp_num) < 6:
-            find(tmp_num)
-
-
-def main():
-    global min_cnt, num_int, btn_set
-
-    num_int = int(input())
-    n = int(input())
-
-    min_cnt = min(min_cnt, abs(100 - num_int))
-    btn_set -= set(map(int, input().split(' '))) if n else set()
-
-    find('') if n < 10 else ''
-    print(min_cnt)
-
-
-main()
-*/
+//python 참고 코드
+//min_cnt = 0x3f3f3f3f
+//num_int = 0
+//btn_set = {i for i in range(0, 10)}
+//
+//
+//def find(num):
+//    global min_cnt, num_int, btn_set
+//
+//    for btn in btn_set:
+//        tmp_num = num + str(btn)
+//        min_cnt = min(min_cnt, abs(num_int - int(tmp_num)) + len(tmp_num))
+//
+//        if len(tmp_num) < 6:
+//            find(tmp_num)
+//
+//
+//def main():
+//    global min_cnt, num_int, btn_set
+//
+//    num_int = int(input())
+//    n = int(input())
+//
+//    min_cnt = min(min_cnt, abs(100 - num_int))
+//    btn_set -= set(map(int, input().split(' '))) if n else set()
+//
+//    find('') if n < 10 else ''
+//    print(min_cnt)
+//
+//
+//main()
