aa079949f40c91010028fecd6bc109503e8abf61
JI SOO <dlwltn815@gmail.com>
2022-04-17 14:42:16 +0900

    3085 사탕게임 문제 풀이

--- Algorithm/Boj/3085_사탕게임/main.swift
+++ Algorithm/Boj/3085_사탕게임/main.swift
@@ -17,11 +17,14 @@ for _ in 0..<n {
 for i in 0..<n {
     for j in 0..<n-1 {
     
+        // 자리 바꿔서 result 갱신해주고
         input[i].swapAt(j,j+1)
         row_count()
         col_count()
+        //다시 자리 바꿔줌
         input[i].swapAt(j,j+1)
         
+        //열끼리 자리 바꿀건데, 값을 temp 에 저장해뒀다가 값을 줘야해.
         var temp = input[j][i]
         input[j][i] = input[j+1][i]
         input[j+1][i] = temp
@@ -29,6 +32,7 @@ for i in 0..<n {
         row_count()
         col_count()
         
+        // 원래자리로 돌려
         temp = input[j][i]
         input[j][i] = input[j+1][i]
         input[j+1][i] = temp
