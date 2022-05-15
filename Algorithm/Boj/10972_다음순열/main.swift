0ba19b41e0ed7155cbc5cafa8ec9481364c8f865
JI SOO <dlwltn815@gmail.com>
2022-05-15 23:13:34 +0900

    10972 다음순열

---
+++ Algorithm/Boj/10972_다음순열/main.swift
@@ -0,0 +1,10 @@
+var arr = [1,2,3,4,5]
+
+//let n = Int(readline()!)!
+//var arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+if arr.reversed() == arr {
+    print(-1)
+} else {
+    
+}
