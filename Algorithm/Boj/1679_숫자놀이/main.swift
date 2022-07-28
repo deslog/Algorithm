b0242a9e68cd76aabf5ec0476824066e1ff111ee
JI SOO <dlwltn815@gmail.com>
2022-07-28 16:35:46 +0900

    1679 숫자놀이 (메모리초과)

---
+++ Algorithm/Boj/1679_숫자놀이/main.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by LeeJiSoo on 2022/07/28.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+let nums = readLine()!.split(separator: " ").map { Int(String($0))! }
+let k = Int(readLine()!)!
+var dp = Array(repeating: [Int](), count: k+1)
+var visited = Array(repeating: false, count: (nums.max()! * k)+1)
+
+for num in nums {
+    visited[num] = true
+    dp[1].append(num)
+}
+
+for i in 2..<k+1 {
+    let previous = dp[i-1]
+    for p in previous {
+        for num in nums {
+            visited[num+p] = true
+            dp[i].append(num+p)
+        }
+    }
+}
+
+for j in 1..<(nums.max()!*k)+1 {
+    if visited[j] == false {
+        print(j % 2 == 0 ? "holsoon win at \(j)" : "jjaksoon win at \(j)")
+        break
+    }
+}
