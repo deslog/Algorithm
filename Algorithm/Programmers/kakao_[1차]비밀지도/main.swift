10f50f2e05829eb0eec555c7294410bd4138f315
JI SOO <dlwltn815@gmail.com>
2022-08-07 00:48:35 +0900

    kakao 실패율 문제풀이 (5, 9, 22 시간초과)
    - filter 사용

--- Algorithm/Programmers/kakao_[1차]비밀지도/main.swift
+++ Algorithm/Programmers/kakao_[1차]비밀지도/main.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+let N = 5
+let stages = [2, 1, 2, 6, 2, 4, 3, 3]
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var failure = [Int: Double]()
+
+    for i in 1..<N+1 {
+        let failNum = stages.filter({ $0 == i }).count
+        let clearNum = stages.filter({ $0 >= i }).count
+        failure[i] = Double(failNum) / Double(clearNum)
+    }
+    let sortArr = failure.sorted(by: <).sorted(by: { $0.1 > $1.1 })
+//    print(sortArr)
+    let result = sortArr.map{ $0.key }
+//    print(result)
+    return result
+}
+
+solution(N, stages)
