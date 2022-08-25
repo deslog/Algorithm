bbd7ac320e5dda57fd8952bd1eb2a5aae1b7cb12
JI SOO <dlwltn815@gmail.com>
2022-08-26 01:58:50 +0900

    11726 2xn타일링 다시풀기 (dp)

--- Algorithm/Boj/11726_2xn타일링/main.swift
+++ Algorithm/Boj/11726_2xn타일링/main.swift
@@ -2,22 +2,46 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by 이지수 on 2022/03/05.
+//  Created by LeeJiSoo on 2022/08/26.
 //
 
-let n = Int(readLine()!)!
-var dp = [Int](repeating: 0, count: n+1)
+import Foundation
 
+//규칙 잘못 생각함
+//let n = Int(readLine()!)!
+//var dp = Array(repeating: 0, count: n+1)
+//
+//dp[1] = 1
+//dp[2] = 2
+//
+//for i in 3..<n+1 {
+//    if i % 2 == 0 {
+//        dp[i] = dp[i-2]*2
+//    } else {
+//        dp[i] = dp[i-1]
+//    }
+//}
+//
+//print(dp[n])
+
+import Foundation
+
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n+1)
+
+if n >= 2 {
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in 3..<n+1 {
+        dp[i] = (dp[i-2] + dp[i-1]) % 10007
+    }
+}
 
 if n == 1 {
     print(1)
 } else if n == 2 {
     print(2)
 } else {
-    dp[1] = 1
-    dp[2] = 2
-    for i in 3..<n+1 {
-        dp[i] = (dp[i-1] + dp[i-2]) % 10007
-    }
     print(dp[n])
 }
--- Algorithm/Boj/1463_1로 만들기/test.swift
+++
@@ -1,8 +0,0 @@
-//
-//  test.swift
-//  Algorithm
-//
-//  Created by 이지수 on 2022/03/05.
-//
-
-import Foundation
