c8f16ac87b10da5b8390f938c59b0c59a9fc6772
JI SOO <dlwltn815@gmail.com>
2022-09-03 16:19:44 +0900

    kakao 프렌즈4블록 5번 10번 실패

---
+++ Algorithm/Programmers/kakao_프렌즈4블록/main.swift
@@ -0,0 +1,83 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by LeeJiSoo on 2022/09/03.
+//
+
+import Foundation
+
+let m = 4
+let n = 5
+let board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
+// 결과값 : 14
+
+func solution(_ m:Int, _ n:Int, _ board:[String]) -> Int {
+
+    // board를 2차원 배열로 바꿔주기
+    var newBoard = splitBoard(board)
+
+    // 지우는 좌표를 기록하기 위한 set 설정 & return 값 설정
+    var removePoint = Set<[Int]>()
+    var cnt = 0
+
+    // 프렌즈 4블록이 없어질때까지 돌려야하니까, while 문으로 removePoint 조건 검사
+    // 원래 while !removePoint.isEmpty 을 조건으로 주었는데, removePoint는 매번 초기화해주니까 이 조건은 안되는구나를 깨닳음
+    while true {
+        // 4블록 위치 찾아서 removePoint에 넣어주기
+        for i in 0..<m-1 {
+            for j in 0..<n-1 {
+                let point = newBoard[i][j]
+                // 만약 point 가 - 라면, 이미 제거된 위치이므로 continue
+                if point == "-" {
+                    continue
+                }
+                // 사방을 보고 다 point 와 같다면 removePoint에 해당 좌표 추가
+                if newBoard[i][j+1] == point, newBoard[i+1][j] == point, newBoard[i+1][j+1] == point {
+                    removePoint.insert([i, j])
+                    removePoint.insert([i, j+1])
+                    removePoint.insert([i+1, j])
+                    removePoint.insert([i+1, j+1])
+                }
+            }
+        }
+
+        // removiPoint가 있으면 cnt 더해주고 블록 지우고, 없으면 끝 cnt 출력
+        if !removePoint.isEmpty {
+            cnt += removePoint.count
+            for point in removePoint {
+                newBoard[point[0]][point[1]] = "-"
+            }
+            removePoint = Set<[Int]>()
+        } else {
+            break
+        }
+
+        // 지금 블록 없는 곳이 "-"인데, 블록을 차례대로 밑으로 내려줘야함
+        for i in 0..<m-1 {
+            for j in 0..<n {
+                let block = newBoard[i][j]
+                let under = newBoard[i+1][j]
+                if under == "-" {
+                    newBoard[i][j] = "-"
+                    newBoard[i+1][j] = block
+                } else {
+                    continue
+                }
+            }
+        }
+    }
+    return cnt
+}
+
+
+func splitBoard(_ board: [String]) -> [[String]] {
+    var newBoard = [[String]]()
+    for b in board  {
+        let arr = b.map { String($0) }
+        newBoard.append(arr)
+    }
+    return newBoard
+}
+
+print(solution(m, n, board))
