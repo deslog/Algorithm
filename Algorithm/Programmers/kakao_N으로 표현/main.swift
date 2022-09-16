8200aedb9005871b900bd5056d2de8e16ac93290
JI SOO <dlwltn815@gmail.com>
2022-09-16 18:17:45 +0900

    wip N으로표현 푸는 중

---
+++ Algorithm/Programmers/kakao_N으로 표현/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by LeeJiSoo on 2022/09/15.
+//
+
+import Foundation
+
+let n = 5
+let number = 12
+// return 4
+
+func solution(_ N:Int, _ number:Int) -> Int {
+    var dp = Array(repeating: Set<Int>(), count: 9)
+    var answer = 9999
+
+    for i in 1..<9 { // 8보다 큰 건 볼 필요가 없음
+        for j in 1..<i { // dp[i]
+            for 
+
+        }
+
+    }
+
+    return answer
+}
