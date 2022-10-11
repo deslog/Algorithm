ef41b3629105ed8fdbe9f85e318667201f15df2c
JI SOO <dlwltn815@gmail.com>
2022-10-11 21:58:53 +0900

    프로그래머스 이진변환반복하기(lv.2) 문제풀이

--- Algorithm/Programmers/이진변환반복하기(lv.2)/main.swift
+++ Algorithm/Programmers/이진변환반복하기(lv.2)/main.swift
@@ -13,54 +13,57 @@ func solution(_ s: String) -> [Int] {
     var zerocount = 0
     var rotation = 0
     var copyS = s
-    var deletezero = 0
-    var ans = [Int]()
 
-    while true {
+    while copyS != "1" {
+        let deleteZeroNum = copyS.filter { $0 == "1" }
+        zerocount += copyS.count - deleteZeroNum.count
+        copyS = String(deleteZeroNum.count, radix: 2)
         rotation += 1
-        print(rotation)
-        // 0제거한 길이 구하고, count에 제거된 0개수 더해주기
-        deletezero = deleteZeroCount(copyS)
-        zerocount += copyS.count - deletezero
-
-        // deletezero 이진법으로 바꾸기
-        let result = makeBinaryScale(deletezero)
-
-        if result != "1" {
-            copyS = result
-        } else {
-            break
-        }
     }
-    ans.append(rotation)
-    ans.append(zerocount)
-    return ans
-}
-
-func deleteZeroCount(_ s: String) -> Int {
-    var result = s.components(separatedBy: "0").joined().count
-    return result
-}
-
-func makeBinaryScale(_ num: Int) -> String {
-    var binaryNum = num
-    var result = ""
-    while true {
-        print("여기지롱")
-        let quot = binaryNum / 2
-        let remainder = binaryNum % 2
-
-        if quot != 1 {
-            result += String(remainder)
-        } else {
-            result += String(remainder)
-            result += "1"
-            break
-        }
-        binaryNum = quot
-    }
-
-    return String(result.reversed())
+    return [rotation, zerocount]
 }
 
 print(solution(s))
+
+//    while result != "10" {
+//        rotation += 1
+//        var deleteZeroCount = copyS.components(separatedBy: "0").joined().count
+//        zerocount += copyS.count - deleteZeroCount
+//        var result = String(deleteZeroCount, radix: 2)
+//        if result == "10" {
+//            break
+//        }
+//        print("result는 \(result)")
+//        print(type(of: result))
+//        copyS = result
+//    }
+
+//    return [rotation, zerocount]
+//}
+
+//func deleteZeroCount(_ s: String) -> Int {
+//    var result = s.components(separatedBy: "0").joined().count
+//    return result
+//}
+
+//func makeBinaryScale(_ num: Int) -> String {
+//    var binaryNum = num
+    //    var result = ""
+    //    while true {
+    //        let quot = binaryNum / 2
+    //        let remainder = binaryNum % 2
+    //
+    //        if quot != 1 {
+    //            result += String(remainder)
+    //            binaryNum = quot
+    //        } else if quot == 1 {
+    //            result += String(remainder)
+    //            result += "1"
+    //            break
+    //        }
+    //    }
+    //
+    //    return String(result.reversed())
+//}
+
+//print(makeBinaryScale(111))
