41f26aa2fe7842441dfd4cf9589d3b021ea24a7b
JI SOO <dlwltn815@gmail.com>
2022-12-28 15:55:42 +0900

    3085 사탕게임 문제풀이

--- Algorithm/Boj/3085_사탕게임/main.swift
+++ Algorithm/Boj/3085_사탕게임/main.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+
+private func solution() {
+    var result = [Int]()
+    let n = Int(readLine()!)!
+    var candy = [Array<Character>]()
+    for _ in 0..<n {
+        candy.append(Array(readLine()!))
+    }
+
+    for i in 0..<n {
+        for j in 0..<n-1 {
+
+            // 행끼리 바꿈
+            candy[i].swapAt(j, j+1)
+            result.append(colCount(n: n, candy: candy))
+            result.append(rowCount(n: n, candy: candy))
+            candy[i].swapAt(j, j+1)
+
+            // 열바꿈
+            var temp = candy[j][i]
+            candy[j][i] = candy[j+1][i]
+            candy[j+1][i] = temp
+            result.append(colCount(n: n, candy: candy))
+            result.append(rowCount(n: n, candy: candy))
+            temp = candy[j][i]
+            candy[j][i] = candy[j+1][i]
+            candy[j+1][i] = temp
+        }
+    }
+    print(result.max()!)
+}
+
+private func colCount(n: Int, candy: [Array<Character>]) -> Int {
+    var result = -999
+    for i in 0..<n {
+        var count = 1
+        for j in 0..<n-1 {
+            if candy[i][j] == candy[i][j+1] {
+                count += 1
+            } else {
+                result = max(result, count)
+                count = 1
+            }
+        }
+        result = max(result, count)
+    }
+    return result
+}
+
+private func rowCount(n: Int, candy: [Array<Character>]) -> Int {
+    var result = -999
+    for i in 0..<n {
+        var count = 1
+        for j in 0..<n-1 {
+            if candy[j][i] == candy[j+1][i] {
+                count += 1
+            } else {
+                result = max(result, count)
+                count = 1
+            }
+        }
+        result = max(result, count)
+    }
+    return result
+}
+
+solution()
