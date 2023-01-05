75173cbc17955d51f642d78fb0f7e806a314773d
JI SOO <dlwltn815@gmail.com>
2023-01-05 14:56:12 +0900

    [RENAME] 123더하기 문제 다시 풀기 위해서 파일명 변경

--- Algorithm/Boj/9095_1,2,3 더하기/main.swift
+++ Algorithm/Boj/9095_1,2,3 더하기/main.swift
@@ -2,30 +2,7 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by 이지수 on 2022/03/07.
+//  Created by LeeJiSoo on 2023/01/05.
 //
 
-let n = Int(readLine()!)!
-
-for _ in 0..<n {
-    let num = Int(readLine()!)!
-    var dp = [Int](repeating: 0, count: num+1)
-    
-    if num == 1 {
-        print(1)
-    } else if num == 2 {
-        print(2)
-    } else if num == 3 {
-        print(4)
-    } else {
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
-
-        for i in 4..<num+1 {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-        }
-        print(dp[num])
-    }
-
-}
+import Foundation
