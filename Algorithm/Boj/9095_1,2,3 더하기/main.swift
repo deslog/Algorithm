1db3a4ad3eefacf41ee5530b77dec65e1e88e8cb
JI SOO <dlwltn815@gmail.com>
2023-01-07 12:41:00 +0900

    123더하기 두번째 문제 풀이

--- Algorithm/Boj/9095_1,2,3 더하기/main.swift
+++ Algorithm/Boj/9095_1,2,3 더하기/main.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+let T = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 12)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in 4..<12 {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+}
+
+for _ in 0..<T {
+    let n = Int(readLine()!)!
+    print(dp[n])
+}
