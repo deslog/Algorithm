b6b271629befb1e140b5d2d2020fd672d1d0fba2
JI SOO <dlwltn815@gmail.com>
2023-01-20 11:55:00 +0900

    14501 퇴사 두번째 문제 풀이

--- Algorithm/Boj/14501_퇴사/main.swift
+++ Algorithm/Boj/14501_퇴사/main.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+let n = Int(readLine()!)!
+var arr = [[0, 0]]
+for _ in 0..<n {
+    arr.append(readLine()!.split(separator: " ").map{ Int($0)! })
+}
+var dp = Array(repeating: 0, count: 100)
+
+for i in 1..<n+1 {
+    if dp[i] > dp[i+1] {
+        dp[i+1] = dp[i]
+    }
+
+    if i+arr[i][0] > n+1 {
+        continue
+    }
+
+    dp[i+arr[i][0]] = max(arr[i][1] + dp[i], dp[i+arr[i][0]])
+}
+
+print(dp.max()!)
