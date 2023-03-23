9bec6ee8bfe826207c3e78471738f5fb507e692d
JI SOO <dlwltn815@gmail.com>
2023-03-23 21:50:07 +0900

    [Solve] boj 1300 k번째 수 문제풀이 완료

---
+++ Algorithm/Boj/1300_K번째 수/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by LeeJiSoo on 2023/03/23.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+let k = Int(readLine()!)!
+
+private func solution() {
+    var left = 0
+    var right = k
+    var answer = 0
+
+    while left <= right {
+        let mid = (left + right) / 2
+        var minCount = 0
+
+        for i in 1..<n+1 {
+            minCount += min(mid / i, n)
+        }
+
+        // 여기 범위가지고 노는게 힘드네
+        // min <= k 일때 answer = mid 로 갱신해주면 안되는데
+        // 왜 항상 크거나 같을때 갱신해줘야하는거지?
+        if minCount < k {
+            left = mid + 1
+        } else {
+            answer = mid
+            right = mid - 1
+        }
+    }
+
+    print(answer)
+}
+
+solution()
