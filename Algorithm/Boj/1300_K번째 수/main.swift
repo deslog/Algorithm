fc0eff32796b86bb8f1c674fd85c48c45334c9b8
JI SOO <dlwltn815@gmail.com>
2023-03-24 02:50:54 +0900

    [Comment] 고민했던 부분 포스팅 주소 첨부

--- Algorithm/Boj/1300_K번째 수/main.swift
+++ Algorithm/Boj/1300_K번째 수/main.swift
@@ -26,6 +26,7 @@ private func solution() {
         // 여기 범위가지고 노는게 힘드네
         // min <= k 일때 answer = mid 로 갱신해주면 안되는데
         // 왜 항상 크거나 같을때 갱신해줘야하는거지?
+        // 이유 -> 감자블로그에 포스팅 완료 https://didu-story.tistory.com/437
         if minCount < k {
             left = mid + 1
         } else {
