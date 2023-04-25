9983e603439aaa452601d4ff79cfce9ca1443ab4
JI SOO <dlwltn815@gmail.com>
2023-04-25 19:27:52 +0900

    [Solve] boj 1541 잃어버린 괄호 (그리디) 문제풀이 완료

--- Algorithm/Boj/1541_잃어버린 괄호/main.swift
+++ Algorithm/Boj/1541_잃어버린 괄호/main.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+func solution() {
+    let input = readLine()!
+    var minus = input.split(separator: "-").map{ String($0) }
+
+    for i in 0..<minus.count {
+        if minus[i].contains("+") {
+            let temp = minus[i].split(separator: "+").map{ Int($0)! }
+            minus[i] = String(temp.reduce(0, +))
+        }
+    }
+
+    var answer = minus.map{ Int($0)! }.reduce(0, -)
+    print(answer + 2 * Int(minus[0])!)
+}
+
+solution()
